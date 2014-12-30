f591f2d3153494ae104331c8c82208bd826283a1
Ran Davidovitz <ran.davidovitz@gmail.com>
2014-12-29 22:27:22 -0500

    Initial

---
+++ README.md
@@ -0,0 +1,21 @@
+angularGoogleApiWrapper
+=======================
+
+This project was created to enable quick integration between angular application and google API
+It will handle the correct loading of google client API for you by injecting code & Scripts.
+
+Version 0.0.1 support the following:
+
+1. Public API access (no OAuth yet)
+2. YouTubeAPI with 1 method for getting channel activity 
+3. Additional services, methods can be created in seconds with the ConstructAPICallMethod function 
+
+Installation
+------------
+
+````
+bower install angular-google-api-wrapper
+````
+
+
+add "angularGoogleApiWrapper" as a dependency.
---
+++ src/angular-google-api-wrapper.js
@@ -0,0 +1,84 @@
+/*jslint plusplus: true, vars: true*/
+/*global angular:true, window:true, gapi: true */
+(function () {
+	'use strict';
+	
+	function GapiService($q, $rootScope, $window, apiKey) {
+		var service = $rootScope.$new(true);
+		service.gapiServiceReady = $q.defer();
+
+		$window.angularGoogleApiWrapperInit = function () {
+			service.initGapi();
+		};
+
+		var tag = document.createElement('script');
+		tag.src = "https://apis.google.com/js/client.js?onload=angularGoogleApiWrapperInit";
+		var firstScriptTag = document.getElementsByTagName('script')[0];
+		firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+		service.initGapi = function () {
+			gapi.client.setApiKey(apiKey);
+			service.$apply(service.gapiServiceReady.resolve());
+		};
+		
+		function returnFunctionByName(functionName, context) {
+			var namespaces = functionName.split('.');
+			var i;
+			for (i = 0; i < namespaces.length; i++) {
+				context = context[namespaces[i]];
+			}
+			return context;
+		}
+		
+		service.ConstructAPICallMethod = function (apiReaderPromise, method) {
+			return function (args) {
+				var deferred = $q.defer();
+				
+				apiReaderPromise.promise.then(function () {
+					var request = returnFunctionByName(method, $window)(args);
+
+					request.execute(function (data) {
+						deferred.resolve(data);
+					}, function (response) {
+						deferred.reject(response);
+					});
+				});
+
+				return deferred.promise;
+			};
+		};
+
+		return service;
+	}
+	GapiService.$inject = ['$q', '$rootScope', '$window', 'apiKey'];
+	
+	function GapiServiceProvider() {
+		var apiKey = null;
+		
+		this.setApiKey = function (newKey) {
+			apiKey = newKey;
+		};
+		
+		this.$get = ['$q', '$rootScope', '$window', function GapiServiceFactory($q, $rootScope, $window) {
+			if (apiKey === null) {
+				throw ("Please set key before callign webservice");
+			}
+			return new GapiService($q, $rootScope, $window, apiKey);
+		}];
+	}
+	
+	angular.module('angularGoogleApiWrapper', [])
+		.provider('gapiService', GapiServiceProvider)
+		.service('googleYouTubeAPI', function ($q, $rootScope, gapiService) {
+			var youtubeAPIready = $q.defer();
+		
+			gapiService.gapiServiceReady.promise.then(function () {
+				gapi.client.load('youtube', 'v3', function () {
+					youtubeAPIready.resolve();
+				});
+			});
+
+			this.activities = {};
+			this.activities.list = gapiService.ConstructAPICallMethod(youtubeAPIready, 'gapi.client.youtube.activities.list');
+		});
+}());
